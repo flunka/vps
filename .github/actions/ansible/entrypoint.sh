e582f06846bde9c5d300625aac85e4a97a72fb92
Piotr Banas <flunka@gmail.com>
2021-05-03 22:06:04 +0200

    Add known hosts

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
+#Set ssh private key
 mkdir ~/.ssh
 echo "$SSH_PRIVATE" > ~/.ssh/ansible
 chmod 0600 ~/.ssh/ansible
 eval `ssh-agent -s`
 echo "$SSH_KEY" | ssh-add ~/.ssh/ansible
-
+# Set known hosts
+mkdir /root/.ssh
+echo "$SSH_KNOWN_HOSTS" > /root/.ssh/known_hosts
+chmod 0600 /root/.ssh/known_hosts
+# Install node exporter role
 ansible-galaxy install cloudalchemy.node_exporter
-
+# Run playbook
 echo "$SUDO_PASS" | ansible-playbook -i $ANSIBLE_INVENTORY $ANSIBLE_PLAYBOOK -K
